e21843761ee39c5ea2b5fe61aade9a7dc89f9ea6
Solikhin <solikhinkhin645@gmail.com>
2024-03-19 13:20:29 +0700

    Pertemuan 5 - Praktikum 3 Fungsi dalam Fungsi

--- Jobsheet5/fungsi.php
+++ Jobsheet5/fungsi.php
@@ -1,20 +1,33 @@
 <?php
 // Membuat fungsi
-function perkenalan($nama, $salam= "Assalamualaikum") {
-    echo $salam.", ";
-    echo "Perkenalkan, nama saya".$nama."<br/> ";
-    echo "Senang berkenalan dengan Anda <br/> ";
-}
+// function perkenalan($nama, $salam = "Assalamualaikum") {
+//    echo $salam . ", ";
+// echo "Perkenalkan, nama saya " . $nama . "<br/> ";
+//  echo "Senang berkenalan dengan Anda <br/> ";
+// }
 
 // Memanggil fungsi yang sudah dibuat
-perkenalan(" Solikhin", "Hallo");
+// perkenalan(" Solikhin", "Hallo");
 
-echo "<hr>";
+// echo "<hr>";
 
-$saya = " Khin ";
-$ucapanSalam = "Selamat pagi";
+// $saya = " Khin ";
+// $ucapanSalam = "Selamat pagi";
 
-// memanggil lagi
-perkenalan($saya)
+// // // Memanggil fungsi lagi
+// // perkenalan($saya);
 
+// membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+function perkenalan($nama, $salam = "Assalamualaikum") {
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/> ";
+// memanggil fungsi lain
+echo "Saya Berusia " . hitungUmur(2003, 2024) . " tahun"; 
+echo "Senang berkenalan dengan Anda <br/> ";
+}
+perkenalan("Khin");
 ?>
