8a15eb57aa106636d1f4512ecf57ad563a401295
Solikhin <solikhinkhin645@gmail.com>
2024-03-19 14:30:25 +0700

    Pertemuan 5 - Praktikum 7 html & php

---
+++ Jobsheet5/php_html_Cara1&2.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title>cara 01</title>
+</head>
+
+<body>
+    <p>Tanggal Hari ini :<?php echo date ("d M Y")?>
+    <p>
+</body>
+
+</html>
+
+<!-- cara 2 -->
+<?php
+echo '<html>';
+echo '<head><title>Cara 02</title></head>';
+echo '<body>';
+echo '<p>Tanggal Hari ini: ' .date('d M Y') .'</p>';
+echo '</body>';
+echo '</html>';
+?>
