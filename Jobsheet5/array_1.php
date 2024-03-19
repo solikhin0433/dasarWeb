5bc225f6adda940e2c45ea18beef1d3c603d704e
Solikhin <solikhinkhin645@gmail.com>
2024-03-19 11:19:39 +0700

    Pertemuan 5 - Praktikum 1

---
+++ Jobsheet5/array_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <h2>Array Terindeks</h2>
+
+    <?php
+$Listdosen = array("Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha");
+echo $Listdosen[2] . "<br>";
+echo $Listdosen[0] . "<br>";
+echo $Listdosen[1] . "<br>";
+?>
+</body>
+
+</html>
