3002663f212a5fb20a5ef515ea312a0424fedb56
Solikhin <solikhinkhin645@gmail.com>
2024-03-19 12:56:30 +0700

    Pertemuan 5 - Praktikum 3 fungsi dengan parameter

---
+++ Jobsheet5/fungsi.php
@@ -0,0 +1,20 @@
+<?php
+// Membuat fungsi
+function perkenalan($nama, $salam) {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya".$nama."<br/> ";
+    echo "Senang berkenalan dengan Anda <br/> ";
+}
+
+// Memanggil fungsi yang sudah dibuat
+perkenalan(" Solikhin", "Hallo");
+
+echo "<hr>";
+
+$saya = " Khin ";
+$ucapanSalam = "Selamat pagi";
+
+// memanggil lagi
+perkenalan($saya, $ucapanSalam)
+
+?>
