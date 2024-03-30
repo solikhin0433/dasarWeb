b3f06361ba5467d08888728d3e47be2d4d5910e3
Solikhin <solikhinkhin645@gmail.com>
2024-03-31 01:58:47 +0700

    Pertemuan 7 - Praktikum 2 Function empty

---
+++ Jobsheet7/empty.php
@@ -0,0 +1,21 @@
+<?php
+// $nama = ""; // Variabel string kosong
+// if(empty($nama)){
+//     echo "<br> Nama tidak terdifinisi atau kosong";
+// }
+// else {
+//     echo "Nama terdefinisi";
+// }
+$myArray = array(); // Array kosong
+if(empty($myArray)){
+    echo " Array tidak terdifinisi atau kosong";
+}
+else {
+    echo "Array terdefinisi dan tidak kosong";
+}
+if (empty ($nonExistentVar)){
+    echo "<br> Variable tidak terdefinisi atau kosong";
+}else{
+    echo "Variable terdefinisi dan tidak kosong";
+}
+?>
