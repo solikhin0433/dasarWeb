0137058a4e7b55909632cffc511a2644146216ee
Solikhin <solikhinkhin645@gmail.com>
2024-04-02 13:12:35 +0700

    Pertemuan 8 - Praktikum 4 Menghias Upload File

--- Jobsheet8/upload.js
+++ Jobsheet8/upload.js
@@ -42,30 +42,33 @@
 // });
 
 // PRAKTIKUM 4 MENGHIAS UPLOAD FILE
-$(document).ready(function () { 
-    $('#file').change(function () {
-        if (this.files.length > 0) {
-            $('#upload-button').prop('disabled', false).css('opacity', 1);
+$(document) .ready(function() {
+    $("#file") .change(function() {
+        if (this.file.lenght > 0) {
+            $("#upload-button") .prop("disabled", false) .css("opacity" , 1);
         } else {
-            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+            $("#upload-button") .prop("disabled", true) .css("opacity" , 0.5);
         }
-    });
-    $('#upload-form').submit(function (e) {
+    })
+    
+    $("#upload-form") .submit(function(e) {
         e.preventDefault();
+
         var formData = new FormData(this);
+
         $.ajax({
-            type: 'POST',
-            url: 'upload_ajax.php',
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: function (response) {
-                $('#status').html(response);   
+            type : "POST",
+            url : "upload_ajax.php",
+            data : formData,
+            Cache : false,
+            contentType : false,
+            processData : false,
+            success : function(response) {
+                $("#status") . html(response);
             },
-            error: function () {
-                $('#status').html('Terjadi kesalahan saat mengunggah file.');
+            error : function() {
+                $("#status") .html("Terjadi kesalahan saat mengunggah file.");
             }
-        });
-    });
-});
+        })
+    })
+})
