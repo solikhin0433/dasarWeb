577e445c8652166c88c7cf98636f39fe9fd51e3e
Solikhin <solikhinkhin645@gmail.com>
2024-04-09 11:43:48 +0700

    Pertemuan 9 - Praktikum 1 membuat tabel user

---
+++ Jobsheet9/membuat.php
@@ -0,0 +1,23 @@
+<?php
+// Koneksi ke database
+$koneksi = mysqli_connect("localhost", "root", "", "prakwebdb");
+// Periksa koneksi
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal: " . mysqli_connect_error();
+    exit();
+}
+// Query untuk membuat tabel user
+$query = "CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+)";
+// Eksekusi query
+if (mysqli_query($koneksi, $query)) {
+    echo "Tabel user berhasil dibuat.";
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
+}
+// Tutup koneksi
+mysqli_close($koneksi);
+?>
