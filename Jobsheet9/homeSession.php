c6fca5334051d080a8fb562090d50a63dbe948b0
Solikhin <solikhinkhin645@gmail.com>
2024-04-10 00:28:45 +0700

    Pertemuan 9 - Praktikum 10 Penerapan Session

---
+++ Jobsheet9/homeSession.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+</head>
+
+<body>
+    <?php
+    session_start();
+    if($_SESSION["status"] == "login") {
+        echo "Selamat datang " . $_SESSION["username"];
+        ?>
+    <br>
+    <a href="sessionLogout.php">Log Out</a>
+    <?php
+    } else {
+        echo "anda belum login. Silahkan " ?>
+    <a href="sessionLoginForm.html">Log In</a>
+    <?php
+    }
+    
+    ?>
+</body>
+
+</html>
