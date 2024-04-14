c0f08610aa486cb890c6382eaace2983b4057f8e
Solikhin <solikhinkhin645@gmail.com>
2024-04-14 20:08:18 +0700

    Pertemuan 10

--- Jobsheet10/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$koneksi = mysqli_connect("localhost", "root", " ", "prakwebdb");
-
-    if (mysqli_connect_errno()) {
-        die ("Koneksi database gagal: " . mysqli_connect_error());
-    }
-?>
