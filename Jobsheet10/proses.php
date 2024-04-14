51a1ea7cbcef16e2165c9d9cde17fecadd2e3533
Solikhin <solikhinkhin645@gmail.com>
2024-04-14 20:18:16 +0700

    Pertemuan 10 - Praktikum 3 Bagian Update

--- Jobsheet10/proses.php
+++ Jobsheet10/proses.php
@@ -16,6 +16,41 @@ if ($aksi == 'tambah') {
     } else {
         echo "Gagal Menambahkan data: " . mysqli_error($koneksi);
     }
+} elseif($aksi == 'ubah'){
+    if(isset($_POST['id'])){
+        $id = $_POST['id'];
+        $query ="UPDATE anggota SET nama='$nama', jenis_kelamin= '$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id=$id";
+        
+        if(mysqli_query($koneksi, $query)){
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal Mengupdate data: " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid.";
+    }
+}
+else if ($aksi == 'hapus'){
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+
+        $query = "DELETE FROM anggota WHERE id = $id";
+        if(mysqli_query($koneksi,$query)){
+            header("Location: index.php");
+            exit();
+        }
+        else {
+            echo "Gagal menghapus data: " . mysqli_error($koneksi);
+        }
+    }
+    else {
+        echo "id tidak valid";
+    }
+}
+
+else {
+    header("Location: index.php");
 }
 
 mysqli_close($koneksi);
