a3cc58c89de0c8dfd4df1760a57c6345ffa3c054
Solikhin <solikhinkhin645@gmail.com>
2024-04-21 21:51:54 +0700

    Pertemuan 11 - Praktikum 3

--- Jobsheet11/admin/template/header.php
+++ Jobsheet11/admin/template/header.php
@@ -238,7 +238,7 @@
     </svg>
 
     <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Rizky Pratama Yudha</a>
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Solikhin</a>
 
         <ul class="navbar-nav flex-row d-md-none">
             <li class="nav-item text-nowrap">
