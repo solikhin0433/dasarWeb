71c560bf0639d48b7320787d13d559559579d2ef
Solikhin <solikhinkhin645@gmail.com>
2024-04-21 11:59:57 +0700

    Pertemuan 11 - Praktikum 2

--- Jobsheet11/fungsi/anti_injection.php
+++ Jobsheet11/fungsi/anti_injection.php
@@ -1,4 +1,5 @@
 <?php
+// Fungsi antiinjection untuk mencegah serangan SQL injection
     function antiinjection($koneksi, $data) {
         $filter_sql = mysqli_real_escape_string($koneksi, stripcslashes(strip_tags(htmlspecialchars($data, ENT_QUOTES))));
         return $filter_sql;
